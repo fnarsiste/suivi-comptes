03960d83ab6e82414cad1d5cb9c1d17ccd0efc52
Nars Sixte <fnarsiste@gmail.com>
2023-02-11 21:19:04 +0100

    Insertion des fragments

---
+++ src/main/java/bj/tresorbenin/suicom/config/thymeleaf/ThymeleafConfiguration.java
@@ -0,0 +1,17 @@
+package bj.tresorbenin.suicom.config.thymeleaf;
+import org.springframework.context.annotation.Configuration;
+/*
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+*/
+@Configuration
+public class ThymeleafConfiguration {
+
+   /*@Bean
+   public SpringTemplateEngine thymeleafLayoutDialect() {
+      SpringTemplateEngine engine = new SpringTemplateEngine();
+      engine.addDialect(new LayoutDialect());
+      return engine;
+   }*/
+}
