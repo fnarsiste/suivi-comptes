068333e33e4b99ad5865fc3d4052ad4e2507b7f8
Nars Sixte <fnarsiste@gmail.com>
2023-02-12 13:02:27 +0100

    Sweetalert and Datatables

--- src/main/resources/static/assets/pages/datatables.init.js
+++ src/main/resources/static/assets/pages/datatables.init.js
@@ -6,14 +6,5 @@
  */
 
 $(document).ready(function() {
-    $('#datatable').DataTable();
-
-    //Buttons examples
-    var table = $('#datatable-buttons').DataTable({
-        lengthChange: false,
-        buttons: ['copy', 'excel', 'pdf', 'colvis']
-    });
-
-    table.buttons().container()
-        .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+    console.log("Init datatable");
 } );
