885ba74d06c109cc250c9af61bdef13aa6636ea1
kadjemar <kouchoro@gmail.com>
2023-02-15 11:14:51 +0100

    Nouvelles repositories et suppression des entités mal placées

---
+++ src/main/java/bj/tresorbenin/suicom/repositories/ProfilRepository.java
@@ -0,0 +1,7 @@
+package bj.tresorbenin.suicom.repositories;
+
+import bj.tresorbenin.suicom.entities.Profil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilRepository extends JpaRepository<Profil,Long> {
+}
---
+++ src/main/java/bj/tresorbenin/suicom/repositories/ProfilUtilisateurRepository.java
@@ -0,0 +1,7 @@
+package bj.tresorbenin.suicom.repositories;
+
+import bj.tresorbenin.suicom.entities.ProfilUtilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilUtilisateurRepository extends JpaRepository<ProfilUtilisateur,Long> {
+}
