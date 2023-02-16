5e95423160c301eb0838adad9809030eea2800e4
Nars Sixte <fnarsiste@gmail.com>
2023-02-16 07:55:34 +0100

    Suppression de l'ancien repo de Banque.

--- src/main/java/bj/tresorbenin/suicom/repositories/BanqueRepository.java
+++
@@ -1,8 +0,0 @@
-package bj.tresorbenin.suicom.repositories;
-
-
-import bj.tresorbenin.suicom.entities.Banque;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BanqueRepository extends JpaRepository<Banque, Long> {
-}
