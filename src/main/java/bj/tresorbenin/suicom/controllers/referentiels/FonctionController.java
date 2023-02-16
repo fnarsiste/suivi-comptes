2fb89c7a934ac88723ba466a9b2053d5eadeb043
Nars Sixte <fnarsiste@gmail.com>
2023-02-16 13:31:16 +0100

    Référentiel Fonction ajouté

---
+++ src/main/java/bj/tresorbenin/suicom/controllers/referentiels/FonctionController.java
@@ -0,0 +1,74 @@
+package bj.tresorbenin.suicom.controllers.referentiels;
+
+import bj.tresorbenin.suicom.controllers.MasterController;
+import bj.tresorbenin.suicom.entities.referentiels.Fonction;
+import bj.tresorbenin.suicom.services.referentiels.FonctionService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Slf4j
+@Controller
+@SuppressWarnings("all")
+@RequestMapping("/{APP_module:referentiels}/{APP_directory:fonctions}")
+public class FonctionController extends MasterController<Fonction> {
+    @Autowired
+    private FonctionService fonctionService;
+
+    @Override
+    protected void initForm(Model model, String id) throws Exception {
+        super.initForm(model, id);
+    }
+
+    @Override
+    protected void showCreateForm(Model model) throws Exception {
+        pageTitle = "MSG.title.fonctions.create";
+    }
+
+    @Override
+    protected void showUpdateForm(Model model, Fonction entity) throws Exception {
+        pageTitle = "MSG.title.fonctions.edit";
+    }
+
+    @Override
+    public void showList(Model model) throws Exception {
+        pageTitle = "MSG.title.fonctions.list";
+        entities = fonctionService.getAll();
+        super.showList(model);
+    }
+
+    @Override
+    public void insert(Model model, Fonction form) throws Exception {
+        fonctionService.create(form);
+        redirectView();
+    }
+
+    @Override
+    public void update(Model model, Fonction form) throws Exception {
+        fonctionService.update(form);
+        redirectView();
+    }
+
+    @Override
+    public void delete(Model model, Object id) throws Exception {
+        fonctionService.delete(Long.valueOf(id.toString()));
+    }
+
+    @Override
+    public Fonction getById(Object id) {
+        return fonctionService.get(id);
+    }
+
+    @Override
+    protected void find(Model model, HttpServletRequest request, Map<String, String> params) {
+    }
+
+    @Override
+    public void doGetCredentialsSession() {
+    }
+}
--- src/main/java/bj/tresorbenin/suicom/entities/referentiels/Fonction.java
+++ src/main/java/bj/tresorbenin/suicom/entities/referentiels/Fonction.java
@@ -2,8 +2,9 @@ package bj.tresorbenin.suicom.entities.referentiels;
 
 import bj.tresorbenin.suicom.entities.base.NamedEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,9 +12,21 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table(name="fonctions")
+@Table(
+        name = "fonctions",
+        indexes = {
+                @Index(name = "fct_code_ix", columnList = "code"),
+                @Index(name = "fct_libelle_ix", columnList = "libelle")
+        },
+        uniqueConstraints = {
+                @UniqueConstraint(name = "fonctions_uk", columnNames = {"code", "date_cessation"})
+        }
+)
 public class Fonction extends NamedEntity {
-    String code;
+
+    public Fonction(String code, String libelle) {
+        super(libelle);
+        setCode(code);
+    }
 }
