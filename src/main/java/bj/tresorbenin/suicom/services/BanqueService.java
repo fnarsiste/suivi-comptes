f8c57ca602aa7b3eeaea13d6e55529bbf47e0f16
Nars Sixte <fnarsiste@gmail.com>
2023-02-16 07:55:07 +0100

    Banque service redéfini (Nouveau prototype)

--- src/main/java/bj/tresorbenin/suicom/services/BanqueService.java
+++ src/main/java/bj/tresorbenin/suicom/services/BanqueService.java
@@ -1,33 +1,20 @@
 package bj.tresorbenin.suicom.services;
 
 import bj.tresorbenin.suicom.entities.Banque;
-import bj.tresorbenin.suicom.repositories.BanqueRepository;
+import bj.tresorbenin.suicom.repositories.jpa.BanqueRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.List;
-
 @Service
-public class BanqueService implements CrudService<Banque, Long>{
+public class BanqueService extends AbstractBaseService<Banque> {
 
     @Autowired
-    private BanqueRepository repo;
+    @Getter
+    private BanqueRepository repository;
 
-    @Override
-    public List<Banque> findAll() {
-        return repo.findAll();
-    }
-
-    @Override
-    public Banque findById(Long id) {
-        return repo.findById(id).orElse(null);
-    }
-
-    @Override
-    public Banque create(Banque entity) {
-        beforeCreate(entity);
-        return repo.save(entity);
+    public BanqueService() {
+        super(Banque.class);
     }
 
     @Override
@@ -36,33 +23,8 @@ public class BanqueService implements CrudService<Banque, Long>{
         Banque banque = entity.clone();
         banque.setId(null);
         // Ici, supprimons l'ancien de la base
-        deleteById(entity.getId());
+        delete(entity.getId());
         // Créer un nouvel enregistrement a parttir du clone
         return create(banque);
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        endDelete(findById(id));
-    }
-
-    @Override
-    public void delete(Banque entity) {
-        endDelete(entity);
-    }
-
-    private void endDelete(Banque entity) {
-        if(entity == null) return;
-        entity.setDateCessation(new Date());
-        entity.setModifierPar("N/A");
-        repo.save(entity);
-    }
-
-    @Override
-    public void beforeCreate(Banque entity) {
-        entity.setCode(entity.getCode().toUpperCase());
-        entity.setDateCreation(entity.getDateCreation() == null ? new Date() : entity.getDateCreation());
-        //entity.setDateCessation(stringIntoDateWithFormat("31/12/9999", "dd/MM/yyyy"));
-        entity.setModifierPar("N/A");
     }
 }
