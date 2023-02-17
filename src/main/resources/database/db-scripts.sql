66478f85be2b714d4bb6b8c068ab3517349c168a
Nars Sixte <fnarsiste@gmail.com>
2023-02-17 08:42:10 +0100

    Redefine database function of triggers creation

--- src/main/resources/database/db-scripts.sql
+++ src/main/resources/database/db-scripts.sql
@@ -29,95 +29,55 @@ begin
 	return code;
 end;
 $$;
-	
--- fonction d'appel du trigger
-CREATE OR REPLACE FUNCTION banques_create_trigger_func()
+
+-- fonction générique pour la creation des triggers
+CREATE OR REPLACE FUNCTION fn_create_trigger_table()
 RETURNS trigger
 language plpgsql 
 AS $$
-BEGIN
+declare 
+	ttable character varying;
+	prefix character varying;
+	taille integer;
+begin
+	ttable := TG_ARGV[0];
+	prefix := TG_ARGV[1];
+	taille := TG_ARGV[2];
+
 	IF NEW.CODE IS NULL OR CONCAT('', NEW.CODE) = '' then
-		NEW.CODE := create_code(new.id, 'BQ');
+		NEW.CODE := (case when taille = 0 then create_code(new.id, prefix) else create_code(new.id, prefix, taille) end);
 	END IF;
-	IF NEW.date_creation IS NULL then
-		NEW.date_creation = now();
-	END IF;
-	if new.date_cessation is null then 
-		new.date_cessation := 'Infinity'; 
-	end if;
 	IF NEW.modifier_par IS NULL OR NEW.modifier_par = '' then
 		NEW.modifier_par = 'App';
 	END IF;
+	-- traitement a faire pour les différentes tables
+	/*case ttable 
+		when 'banques' then 
+		else 
+	end;*/
+	NEW.date_creation = current_timestamp;
+	new.date_cessation := 'Infinity'; 
 	RETURN NEW;
 END;
 $$;	
-
+	
 -- trigger d'insertion dans banque
 CREATE OR REPLACE TRIGGER TRG_INSERT_BANQUE 
 BEFORE INSERT ON banques 
-FOR EACH ROW EXECUTE PROCEDURE banques_create_trigger_func();
-
-
--- ///////
--- fonction d'appel du trigger
-CREATE OR REPLACE FUNCTION fonctions_create_trigger_func()
-RETURNS trigger
-language plpgsql 
-AS $$
-BEGIN
-	IF NEW.CODE IS NULL OR CONCAT('', NEW.CODE) = '' then
-		NEW.CODE := create_code(new.id, 'FN');
-	END IF;
-	IF NEW.date_creation IS NULL then
-		NEW.date_creation = now();
-	END IF;
-	if new.date_cessation is null then 
-		new.date_cessation := 'Infinity'; 
-	end if;
-	IF NEW.modifier_par IS NULL OR NEW.modifier_par = '' then
-		NEW.modifier_par = 'App';
-	END IF;
-	RETURN NEW;
-END;
-$$;	
+FOR EACH ROW EXECUTE PROCEDURE fn_create_trigger_table('banques', 'BQ', 0);
 
 CREATE OR REPLACE TRIGGER TRG_INSERT_FONCTIONS
 BEFORE INSERT ON fonctions 
-FOR EACH ROW EXECUTE PROCEDURE fonctions_create_trigger_func();
--- ///////
-
-
--- ///////
--- fonction d'appel du trigger
-CREATE OR REPLACE FUNCTION agents_create_trigger_func()
-RETURNS trigger
-language plpgsql 
-AS $$
-BEGIN
-	IF NEW.CODE IS NULL OR CONCAT('', NEW.CODE) = '' then
-		NEW.CODE := create_code(new.id, 'AGT', 16);
-	END IF;
-	IF NEW.date_creation IS NULL then
-		NEW.date_creation = now();
-	END IF;
-	if new.date_cessation is null then 
-		new.date_cessation := 'Infinity'; 
-	end if;
-	IF NEW.modifier_par IS NULL OR NEW.modifier_par = '' then
-		NEW.modifier_par = 'App';
-	END IF;
-	RETURN NEW;
-END;
-$$;	
+FOR EACH ROW EXECUTE PROCEDURE fn_create_trigger_table('fonctions', 'FN', 0);
 
 CREATE OR REPLACE TRIGGER TRG_INSERT_AGENTS
 BEFORE INSERT ON agents 
-FOR EACH ROW EXECUTE PROCEDURE agents_create_trigger_func();
+FOR EACH ROW EXECUTE PROCEDURE fn_create_trigger_table('agents', 'AGT', 16);
 
--- ///////
+CREATE OR REPLACE TRIGGER TRG_INSERT_STATUT
+BEFORE INSERT ON statuts
+FOR EACH ROW EXECUTE PROCEDURE fn_create_trigger_table('statuts', 'ST', 0);
 
--- Auto-generated SQL script #202302151750
-INSERT INTO banques (code,date_cessation,date_creation,modifier_par,libelle,adresse,sigle)
-	VALUES ('BQ-0000000000004', now(),'2023-02-15 17:45:58.570','N/A','BANK OF AFRICA','','BOA');
-
-
+CREATE OR REPLACE TRIGGER TRG_INSERT_PROFILS
+BEFORE INSERT ON profils
+FOR EACH ROW EXECUTE PROCEDURE fn_create_trigger_table('profils', 'PF', 0);
